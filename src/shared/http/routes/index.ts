993f148ed129618abb65e16b6558e5edf09954e3
Fernando <fernandobritto.web@gmail.com>
2022-06-24 09:09:35 -0300

    Initial commit

--- src/shared/http/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import productsRouter from '@modules/products/routes/products.routes'
-
-const routes = Router()
-
-routes.use('/products', productsRouter)
-
-export default routes
