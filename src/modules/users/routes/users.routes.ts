d45707f6b174057d2f5b5cee8a05b1cf06de4c20
Fernando <fernandobritto.web@gmail.com>
2022-07-10 10:28:53 -0300

    feat: create service to upload user avatar

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express'
 import { celebrate, Joi, Segments } from 'celebrate'
+import multer from 'multer'
+import uploadConfig from '@config/upload'
 import UsersController from '../controllers/UsersController'
 import isAuthenticated from '@shared/middlewares/isAuthenticated'
+import UserAvatarController from '../controllers/UserAvatarController'
 
 const usersRouter = Router()
 const usersController = new UsersController()
+const usersAvatarController = new UserAvatarController()
+
+const upload = multer(uploadConfig)
 
 usersRouter.get('/', isAuthenticated, usersController.index)
 
@@ -53,6 +59,13 @@ usersRouter.delete(
     },
   }),
   usersController.delete,
+)
+
+usersRouter.patch(
+  '/avatar',
+  isAuthenticated,
+  upload.single('avatar'),
+  usersAvatarController.update,
 )
 
 export default usersRouter
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,12 +3,14 @@ import express, { Request, Response, NextFunction } from 'express'
 import cors from 'cors'
 import routes from '@routes/index'
 import AppError from '@shared/erros/AppError'
+import uploadConfig from '@config/upload'
 import '@database/index'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use('/files', express.static(uploadConfig.directory))
 
 app.use(routes)
 
@@ -26,6 +28,6 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   })
 })
 
-app.listen(3333, () => {
-  console.log('Server started on port 3000 🏆')
+app.listen(8084, () => {
+  console.log('Server started on port 8084 🏆')
 })
